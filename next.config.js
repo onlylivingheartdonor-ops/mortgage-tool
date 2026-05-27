c79a3cdda02b1294a083dfe6e909e0ed7cdbf1b8
David M. Graham <onlylivingheartdonor.com>
2026-05-27 15:15:24 -0400

    Regenerate lock file for Next.js 16

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  trailingSlash: false,
+  
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'mortgageaffordabilityestimator.com' }],
+        destination: 'https://www.mortgageaffordabilityestimator.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
